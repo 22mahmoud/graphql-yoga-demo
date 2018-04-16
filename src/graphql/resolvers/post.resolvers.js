4cc89a56b0396acb505b9939d54b767b70b2ab3d
22mahmoud <mahmouds1997@yahoo.com>
2018-04-16 15:55:46 +0200

    modularize graphql schema & resolvers

---
+++ src/graphql/resolvers/post.resolvers.js
@@ -0,0 +1,35 @@
+let idCount = 0;
+const posts = [];
+
+export default {
+  Query: {
+    description: () => `This is the API for a simple blogging application`,
+    posts: () => posts,
+    post: (parent, args) => posts.find(post => post.id === args.id)
+  },
+  Mutation: {
+    createDraft: (parent, args) => {
+      const post = {
+        id: `post_${idCount++}`,
+        title: args.title,
+        content: args.content,
+        published: false
+      };
+      posts.push(post);
+      return post;
+    },
+    deletePost: (parent, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+      if (postIndex > -1) {
+        const deleted = posts.splice(postIndex, 1);
+        return deleted[0];
+      }
+      return null;
+    },
+    publish: (parent, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+      posts[postIndex].published = true;
+      return posts[postIndex];
+    }
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import {} from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga";
+import typeDefs from "./graphql/schema";
+import resolvers from "./graphql/resolvers";
 
-const typedefs = `
-    type Query {
-        description: String
-    }
-`;
-
-const resolvers = {
-  Query: {
-    description: () => "this is the API for simple blogging application"
-  }
+const options = {
+  port: 8000,
+  endpoint: "/graphql",
+  playground: "/playground"
 };
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(options, () =>
+  console.log(`Server is running on PORT: ${options.port}`)
+);
